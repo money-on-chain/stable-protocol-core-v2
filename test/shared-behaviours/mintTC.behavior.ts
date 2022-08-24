48dd9d0c6c08b495068ec21f3708c13ba493c93c
fruiz08 <fruiz@atixlabs.com>
2022-08-24 12:32:01 -0300

    chore: remove dynamic test generation

--- test/shared-behaviours/mintTC.behavior.ts
+++ test/shared-behaviours/mintTC.behavior.ts
@@ -17,9 +17,9 @@ const mintTCBehavior = function () {
       ({ alice } = await getNamedAccounts());
     });
     describe("WHEN alice sends 100 rbtc to mint Collateral Token", function () {
-      let alicePrevBalance: Balance;
+      let alicePrevACBalance: Balance;
       beforeEach(async function () {
-        alicePrevBalance = await mocFunctions.acBalanceOf(alice);
+        alicePrevACBalance = await mocFunctions.acBalanceOf(alice);
         await mocFunctions.mintTC(alice, 100);
       });
       it("THEN alice receives 100 Collateral Token", async function () {
@@ -32,9 +32,42 @@ const mintTCBehavior = function () {
         assertPrec(100 * 0.05, await mocFunctions.acBalanceOf(mocFeeFlow));
       });
       it("THEN alice balance decrease 100 rbtc + 5% for Moc Fee Flow", async function () {
-        const aliceActualBalance = await mocFunctions.acBalanceOf(alice);
-        const diff = alicePrevBalance.sub(aliceActualBalance);
+        const aliceActualACBalance = await mocFunctions.acBalanceOf(alice);
+        const diff = alicePrevACBalance.sub(aliceActualACBalance);
         assertPrec(100 * 1.05, diff);
+      });
+      describe("AND alice sends again 100 rbtc to mint Collateral Token", function () {
+        let alicePrevACBalance: Balance;
+        let alicePrevTCBalance: Balance;
+        let mocPrevACBalance: Balance;
+        let mocFeeFlowPrevACBalance: Balance;
+        beforeEach(async function () {
+          alicePrevACBalance = await mocFunctions.acBalanceOf(alice);
+          alicePrevTCBalance = await mocFunctions.tcBalanceOf(alice);
+          mocPrevACBalance = await mocFunctions.acBalanceOf(mocContracts.mocCore.address);
+          mocFeeFlowPrevACBalance = await mocFunctions.acBalanceOf(mocFeeFlow);
+          await mocFunctions.mintTC(alice, 100);
+        });
+        it("THEN alice receives 100 Collateral Token", async function () {
+          const aliceActualTCBalance = await mocFunctions.tcBalanceOf(alice);
+          const diff = aliceActualTCBalance.sub(alicePrevTCBalance);
+          assertPrec(100, diff);
+        });
+        it("THEN Moc balance increase 100 rbtc", async function () {
+          const mocActualACBalance = await mocFunctions.acBalanceOf(mocContracts.mocCore.address);
+          const diff = mocActualACBalance.sub(mocPrevACBalance);
+          assertPrec(100, diff);
+        });
+        it("THEN Moc Fee Flow balance increase 5% of 100 rbtc", async function () {
+          const mocFeeFlowActualACBalance = await mocFunctions.acBalanceOf(mocFeeFlow);
+          const diff = mocFeeFlowActualACBalance.sub(mocFeeFlowPrevACBalance);
+          assertPrec(100 * 0.05, diff);
+        });
+        it("THEN alice balance decrease 100 rbtc + 5% for Moc Fee Flow", async function () {
+          const aliceActualACBalance = await mocFunctions.acBalanceOf(alice);
+          const diff = alicePrevACBalance.sub(aliceActualACBalance);
+          assertPrec(100 * 1.05, diff);
+        });
       });
     });
   });
