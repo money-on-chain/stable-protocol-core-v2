4ef44326523636104f3566a277f11b6cab272e9d
fruiz08 <fruiz@atixlabs.com>
2022-09-09 17:50:07 -0300

    feat: add mocSettlement

--- contracts/MocSettlement.sol
+++ contracts/MocSettlement.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.16;
 import "./governance/MocUpgradable.sol";
 
 contract MocSettlement is MocUpgradable {
-    uint256 public bes; // Cantidad de bloques entre settlements
-    uint256 public bns; // Bloque del siguiente settlement
-    uint256 public bmulcdj; // Multiplicador de bloques de ajuste de cobertura. Cada cuanto se hace el ajuste
-    uint256 public bncdj; // Bloque del siguiente ajuste de cobertura
-
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
+    // ------- Storage -------
+    // number of blocks between settlements
+    uint256 public bes;
+    // next settlement block
+    uint256 public bns;
+    // coverage adjustment block multiplier. How often the adjustment is made
+    uint256 public bmulcdj;
+    // next coverage adjustment block
+    uint256 public bncdj;
 
     // ------- Initializer -------
     /**
@@ -23,6 +23,9 @@ contract MocSettlement is MocUpgradable {
         __MocUpgradable_init(governor_, stopper_);
     }
 
+    /**
+     * @notice get the number of blocks remaining for settlement
+     */
     function getBts() external view returns (uint256) {
         if (block.number >= bns) return 0;
         return bns - block.number;
