0872f8734ec2e5dcb2368b41d6f151d84665d25a
fruiz08 <fruiz@atixlabs.com>
2022-09-09 17:52:36 -0300

    feat: mocSettlement gap

--- contracts/MocSettlement.sol
+++ contracts/MocSettlement.sol
@@ -30,4 +30,11 @@ contract MocSettlement is MocUpgradable {
         if (block.number >= bns) return 0;
         return bns - block.number;
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
