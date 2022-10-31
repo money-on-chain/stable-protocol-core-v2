6de90baab92b7be13782e57728e73690df0fe8d7
jmendiola222 <julian.mendiola@iohk.io>
2022-10-31 07:28:14 -0300

    feat: add edit pegged token changer template

--- contracts/governance/changerTemplates/AddPeggedTokenChangerTemplate.sol
+++ contracts/governance/changerTemplates/AddPeggedTokenChangerTemplate.sol
@@ -4,9 +4,9 @@ import "../../interfaces/IChangeContract.sol";
 import "../../core/MocCore.sol";
 
 /**
-  @title AddPeggedTokenChangerTemplate
-  @notice This contract is a ChangeContract intended to be used with Moc Aeropulus 
-  governance system. It allows the addition of a new Pegged Token to the system.
+ * @title AddPeggedTokenChangerTemplate
+ * @notice This contract is a ChangeContract intended to be used with Moc Aeropulus
+ * governance system. It allows the addition of a new Pegged Token to the system.
  */
 contract AddPeggedTokenChangerTemplate is IChangeContract, MocHelper {
     // ------- Storage -------
@@ -14,10 +14,10 @@ contract AddPeggedTokenChangerTemplate is IChangeContract, MocHelper {
     MocCore public mocCore;
     MocCore.PeggedTokenParams internal peggedTokenParams;
 
-    /** 
-    @notice Constructor
-    @param mocCore_ Address of the contract to add Pegged Token to
-  */
+    /**
+     * @notice Constructor
+     * @param mocCore_ Address of the contract to add Pegged Token to
+     */
     constructor(MocCore mocCore_, MocCore.PeggedTokenParams memory peggedTokenParams_) {
         mocCore = mocCore_;
         peggedTokenParams = peggedTokenParams_;
@@ -41,10 +41,17 @@ contract AddPeggedTokenChangerTemplate is IChangeContract, MocHelper {
     }
 
     /**
-    @notice Execute the changes.
-    @dev Should be called by the governor, but this contract does not check that explicitly
-    because it is not its responsibility in the current architecture
-   */
+     * @notice Returns the Pegged Token Params configurations that's going to be added
+     */
+    function getPeggedTokenParams() public view returns (MocCore.PeggedTokenParams memory) {
+        return peggedTokenParams;
+    }
+
+    /**
+     * @notice Execute the changes.
+     * @dev Should be called by the governor, but this contract does not check that explicitly
+     * because it is not its responsibility in the current architecture
+     */
     function execute() external {
         mocCore.addPeggedToken(peggedTokenParams);
     }
