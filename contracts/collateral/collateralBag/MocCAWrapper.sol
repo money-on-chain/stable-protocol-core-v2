72ba8f6f78a054225b2381db628905f060a6463f
fruiz08 <fruiz@atixlabs.com>
2022-12-16 12:48:02 -0300

    feat: stores Asset decimal and truncate if less than 18

--- contracts/collateral/collateralBag/MocCAWrapper.sol
+++ contracts/collateral/collateralBag/MocCAWrapper.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.17;
 
 import "../../governance/MocUpgradable.sol";
 import "../rc20/MocCARC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 /**
  * @title MocCAWrapper: Moc Collateral Asset Wrapper
@@ -100,6 +100,8 @@ contract MocCAWrapper is MocUpgradable {
         uint256 index;
         // true if asset token exists
         bool exists;
+        // how many decimals differs from 18. Eg: if decimals are 6, shift is 12
+        int8 shift;
     }
 
     // ------- Storage -------
@@ -209,9 +211,27 @@ contract MocCAWrapper is MocUpgradable {
         // divide currencyNeeded by asset price to get how many assets we need
         // [N] = [PREC] / [PREC]
         assetAmount = currencyNeeded / _getAssetPrice(assetAddress_);
-        // convert back to wcaToken to align decimal places and avoir rounding error
-        wcaTokenAmount_ = _convertAssetToToken(assetAddress_, assetAmount);
+        // truncate wcaToken amount to align decimal places and avoid rounding error
+        wcaTokenAmount_ = _truncate(assetAddress_, wcaTokenAmount_);
         return (assetAmount, wcaTokenAmount_);
+    }
+
+    /**
+     * @notice truncate amount of decimals corresponding to the Asset shift value
+     * @dev this is necessary to avoid rounding errors when dealing with Assets with decimal places less than 18
+     *  Eg: Asset decimals = 6; value = 10999999 => truncated = 10999900
+     * @param assetAddress_ Asset contract address
+     * @param value_ number to truncate
+     * @return truncated value truncated
+     */
+    function _truncate(address assetAddress_, uint256 value_) internal view returns (uint256 truncated) {
+        int8 shift = assetIndex[assetAddress_].shift;
+        truncated = value_;
+        if (shift > 0) {
+            truncated /= 10 ** uint8(shift);
+            truncated *= 10 ** uint8(shift);
+        }
+        return truncated;
     }
 
     /**
@@ -662,7 +682,11 @@ contract MocCAWrapper is MocUpgradable {
         (, bool has) = priceProvider_.peek();
         if (!has) revert InvalidAddress();
         if (!assetIndex[address(asset_)].exists) {
-            assetIndex[address(asset_)] = AssetIndex({ index: uint256(assets.length), exists: true });
+            assetIndex[address(asset_)] = AssetIndex({
+                index: uint256(assets.length),
+                exists: true,
+                shift: 18 - int8(IERC20Metadata(address(asset_)).decimals())
+            });
             assets.push(asset_);
         }
         priceProviderMap[address(asset_)] = priceProvider_;
