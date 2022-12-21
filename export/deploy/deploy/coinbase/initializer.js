3a09119abe93a90d703860abe29e26ca7edc764d
fruiz08 <fruiz@atixlabs.com>
2022-12-21 15:12:11 -0300

    feat: remove typechain

--- export/deploy/deploy/coinbase/initializer.js
+++ export/deploy/deploy/coinbase/initializer.js
@@ -1,236 +1,114 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __generator =
-  (this && this.__generator) ||
-  function (thisArg, body) {
-    var _ = {
-        label: 0,
-        sent: function () {
-          if (t[0] & 1) throw t[1];
-          return t[1];
-        },
-        trys: [],
-        ops: [],
-      },
-      f,
-      y,
-      t,
-      g;
-    return (
-      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
-      typeof Symbol === "function" &&
-        (g[Symbol.iterator] = function () {
-          return this;
-        }),
-      g
-    );
-    function verb(n) {
-      return function (v) {
-        return step([n, v]);
-      };
-    }
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
-      if (f) throw new TypeError("Generator is already executing.");
-      while (_)
-        try {
-          if (
-            ((f = 1),
-            y &&
-              (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) &&
-              !(t = t.call(y, op[1])).done)
-          )
-            return t;
-          if (((y = 0), t)) op = [op[0] & 2, t.value];
-          switch (op[0]) {
-            case 0:
-            case 1:
-              t = op;
-              break;
-            case 4:
-              _.label++;
-              return { value: op[1], done: false };
-            case 5:
-              _.label++;
-              y = op[1];
-              op = [0];
-              continue;
-            case 7:
-              op = _.ops.pop();
-              _.trys.pop();
-              continue;
-            default:
-              if (!((t = _.trys), (t = t.length > 0 && t[t.length - 1])) && (op[0] === 6 || op[0] === 2)) {
-                _ = 0;
-                continue;
-              }
-              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
-                _.label = op[1];
-                break;
-              }
-              if (op[0] === 6 && _.label < t[1]) {
-                _.label = t[1];
-                t = op;
-                break;
-              }
-              if (t && _.label < t[2]) {
-                _.label = t[2];
-                _.ops.push(op);
-                break;
-              }
-              if (t[2]) _.ops.pop();
-              _.trys.pop();
-              continue;
-          }
-          op = body.call(thisArg, _);
-        } catch (e) {
-          op = [6, e];
-          y = 0;
-        } finally {
-          f = t = 0;
-        }
-      if (op[0] & 5) throw op[1];
-      return { value: op[0] ? op[1] : void 0, done: true };
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-  };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var hardhat_1 = require("hardhat");
-var typechain_1 = require("../../typechain");
 var utils_1 = require("../../scripts/utils");
-var deployFunc = function (hre) {
-  return __awaiter(void 0, void 0, void 0, function () {
-    var deployments,
-      network,
-      _a,
-      coreParams,
-      settlementParams,
-      feeParams,
-      ctParams,
-      mocAddresses,
-      signer,
-      deployedMocContractProxy,
-      MocCACoinbase,
-      deployedTCContract,
-      CollateralToken,
-      governorAddress,
-      pauserAddress,
-      mocFeeFlowAddress,
-      mocAppreciationBeneficiaryAddress,
-      governorMockFactory;
+var deployFunc = function (hre) { return __awaiter(void 0, void 0, void 0, function () {
+    var deployments, network, _a, coreParams, settlementParams, feeParams, ctParams, mocAddresses, signer, deployedMocContractProxy, MocCACoinbase, deployedTCContract, CollateralToken, governorAddress, pauserAddress, mocFeeFlowAddress, mocAppreciationBeneficiaryAddress, governorMockFactory;
     return __generator(this, function (_b) {
-      switch (_b.label) {
-        case 0:
-          deployments = hre.deployments;
-          network = hre.network.name;
-          (_a = (0, utils_1.getNetworkConfig)({ network: network })),
-            (coreParams = _a.coreParams),
-            (settlementParams = _a.settlementParams),
-            (feeParams = _a.feeParams),
-            (ctParams = _a.ctParams),
-            (mocAddresses = _a.mocAddresses);
-          signer = hardhat_1.ethers.provider.getSigner();
-          return [4 /*yield*/, deployments.getOrNull("MocCACoinbaseProxy")];
-        case 1:
-          deployedMocContractProxy = _b.sent();
-          if (!deployedMocContractProxy) throw new Error("No MocCACoinbaseProxy deployed.");
-          MocCACoinbase = typechain_1.MocCACoinbase__factory.connect(deployedMocContractProxy.address, signer);
-          return [4 /*yield*/, deployments.getOrNull("CollateralTokenCoinbaseProxy")];
-        case 2:
-          deployedTCContract = _b.sent();
-          if (!deployedTCContract) throw new Error("No CollateralTokenCoinbaseProxy deployed.");
-          CollateralToken = typechain_1.MocTC__factory.connect(deployedTCContract.address, signer);
-          (governorAddress = mocAddresses.governorAddress),
-            (pauserAddress = mocAddresses.pauserAddress),
-            (mocFeeFlowAddress = mocAddresses.mocFeeFlowAddress),
-            (mocAppreciationBeneficiaryAddress = mocAddresses.mocAppreciationBeneficiaryAddress);
-          if (!(network == "hardhat")) return [3 /*break*/, 5];
-          return [4 /*yield*/, hardhat_1.ethers.getContractFactory("GovernorMock")];
-        case 3:
-          governorMockFactory = _b.sent();
-          return [4 /*yield*/, governorMockFactory.deploy()];
-        case 4:
-          governorAddress = _b.sent().address;
-          _b.label = 5;
-        case 5:
-          console.log("initializing...");
-          // initializations
-          return [
-            4 /*yield*/,
-            (0, utils_1.waitForTxConfirmation)(
-              CollateralToken.initialize(ctParams.name, ctParams.symbol, MocCACoinbase.address, governorAddress),
-            ),
-          ];
-        case 6:
-          // initializations
-          _b.sent();
-          return [
-            4 /*yield*/,
-            (0, utils_1.waitForTxConfirmation)(
-              MocCACoinbase.initialize(
-                {
-                  initializeBaseBucketParams: {
-                    tcTokenAddress: CollateralToken.address,
-                    mocFeeFlowAddress: mocFeeFlowAddress,
-                    mocAppreciationBeneficiaryAddress: mocAppreciationBeneficiaryAddress,
-                    protThrld: coreParams.protThrld,
-                    liqThrld: coreParams.liqThrld,
-                    feeRetainer: feeParams.feeRetainer,
-                    tcMintFee: feeParams.mintFee,
-                    tcRedeemFee: feeParams.redeemFee,
-                    swapTPforTPFee: feeParams.swapTPforTPFee,
-                    swapTPforTCFee: feeParams.swapTPforTCFee,
-                    swapTCforTPFee: feeParams.swapTCforTPFee,
-                    redeemTCandTPFee: feeParams.redeemTCandTPFee,
-                    mintTCandTPFee: feeParams.mintTCandTPFee,
-                    successFee: coreParams.successFee,
-                    appreciationFactor: coreParams.appreciationFactor,
-                  },
-                  governorAddress: governorAddress,
-                  pauserAddress: pauserAddress,
-                  emaCalculationBlockSpan: coreParams.emaCalculationBlockSpan,
-                  bes: settlementParams.bes,
-                },
-                { gasLimit: utils_1.GAS_LIMIT_PATCH },
-              ),
-            ),
-          ];
-        case 7:
-          _b.sent();
-          console.log("initialization completed!");
-          return [2 /*return*/, hre.network.live]; // prevents re execution on live networks
-      }
+        switch (_b.label) {
+            case 0:
+                deployments = hre.deployments;
+                network = hre.network.name;
+                _a = (0, utils_1.getNetworkConfig)({ network: network }), coreParams = _a.coreParams, settlementParams = _a.settlementParams, feeParams = _a.feeParams, ctParams = _a.ctParams, mocAddresses = _a.mocAddresses;
+                signer = hardhat_1.ethers.provider.getSigner();
+                return [4 /*yield*/, deployments.getOrNull("MocCACoinbaseProxy")];
+            case 1:
+                deployedMocContractProxy = _b.sent();
+                if (!deployedMocContractProxy)
+                    throw new Error("No MocCACoinbaseProxy deployed.");
+                return [4 /*yield*/, hardhat_1.ethers.getContractAt("MocCACoinbase", deployedMocContractProxy.address, signer)];
+            case 2:
+                MocCACoinbase = _b.sent();
+                return [4 /*yield*/, deployments.getOrNull("CollateralTokenCoinbaseProxy")];
+            case 3:
+                deployedTCContract = _b.sent();
+                if (!deployedTCContract)
+                    throw new Error("No CollateralTokenCoinbaseProxy deployed.");
+                return [4 /*yield*/, hardhat_1.ethers.getContractAt("MocTC", deployedTCContract.address, signer)];
+            case 4:
+                CollateralToken = _b.sent();
+                governorAddress = mocAddresses.governorAddress, pauserAddress = mocAddresses.pauserAddress, mocFeeFlowAddress = mocAddresses.mocFeeFlowAddress, mocAppreciationBeneficiaryAddress = mocAddresses.mocAppreciationBeneficiaryAddress;
+                if (!(network == "hardhat")) return [3 /*break*/, 7];
+                return [4 /*yield*/, hardhat_1.ethers.getContractFactory("GovernorMock")];
+            case 5:
+                governorMockFactory = _b.sent();
+                return [4 /*yield*/, governorMockFactory.deploy()];
+            case 6:
+                governorAddress = (_b.sent()).address;
+                _b.label = 7;
+            case 7:
+                console.log("initializing...");
+                // initializations
+                return [4 /*yield*/, (0, utils_1.waitForTxConfirmation)(CollateralToken.initialize(ctParams.name, ctParams.symbol, MocCACoinbase.address, governorAddress))];
+            case 8:
+                // initializations
+                _b.sent();
+                return [4 /*yield*/, (0, utils_1.waitForTxConfirmation)(MocCACoinbase.initialize({
+                        initializeBaseBucketParams: {
+                            tcTokenAddress: CollateralToken.address,
+                            mocFeeFlowAddress: mocFeeFlowAddress,
+                            mocAppreciationBeneficiaryAddress: mocAppreciationBeneficiaryAddress,
+                            protThrld: coreParams.protThrld,
+                            liqThrld: coreParams.liqThrld,
+                            feeRetainer: feeParams.feeRetainer,
+                            tcMintFee: feeParams.mintFee,
+                            tcRedeemFee: feeParams.redeemFee,
+                            swapTPforTPFee: feeParams.swapTPforTPFee,
+                            swapTPforTCFee: feeParams.swapTPforTCFee,
+                            swapTCforTPFee: feeParams.swapTCforTPFee,
+                            redeemTCandTPFee: feeParams.redeemTCandTPFee,
+                            mintTCandTPFee: feeParams.mintTCandTPFee,
+                            successFee: coreParams.successFee,
+                            appreciationFactor: coreParams.appreciationFactor,
+                        },
+                        governorAddress: governorAddress,
+                        pauserAddress: pauserAddress,
+                        emaCalculationBlockSpan: coreParams.emaCalculationBlockSpan,
+                        bes: settlementParams.bes,
+                    }, { gasLimit: utils_1.GAS_LIMIT_PATCH }))];
+            case 9:
+                _b.sent();
+                console.log("initialization completed!");
+                return [2 /*return*/, hre.network.live]; // prevents re execution on live networks
+        }
     });
-  });
-};
+}); };
 exports.default = deployFunc;
 deployFunc.id = "Initialized_Coinbase"; // id required to prevent re-execution
 deployFunc.tags = ["InitializerCoinbase"];
--- export/deploy/scripts/utils.js
+++ export/deploy/scripts/utils.js
@@ -1,211 +1,102 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __generator =
-  (this && this.__generator) ||
-  function (thisArg, body) {
-    var _ = {
-        label: 0,
-        sent: function () {
-          if (t[0] & 1) throw t[1];
-          return t[1];
-        },
-        trys: [],
-        ops: [],
-      },
-      f,
-      y,
-      t,
-      g;
-    return (
-      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
-      typeof Symbol === "function" &&
-        (g[Symbol.iterator] = function () {
-          return this;
-        }),
-      g
-    );
-    function verb(n) {
-      return function (v) {
-        return step([n, v]);
-      };
-    }
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
-      if (f) throw new TypeError("Generator is already executing.");
-      while (_)
-        try {
-          if (
-            ((f = 1),
-            y &&
-              (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) &&
-              !(t = t.call(y, op[1])).done)
-          )
-            return t;
-          if (((y = 0), t)) op = [op[0] & 2, t.value];
-          switch (op[0]) {
-            case 0:
-            case 1:
-              t = op;
-              break;
-            case 4:
-              _.label++;
-              return { value: op[1], done: false };
-            case 5:
-              _.label++;
-              y = op[1];
-              op = [0];
-              continue;
-            case 7:
-              op = _.ops.pop();
-              _.trys.pop();
-              continue;
-            default:
-              if (!((t = _.trys), (t = t.length > 0 && t[t.length - 1])) && (op[0] === 6 || op[0] === 2)) {
-                _ = 0;
-                continue;
-              }
-              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
-                _.label = op[1];
-                break;
-              }
-              if (op[0] === 6 && _.label < t[1]) {
-                _.label = t[1];
-                t = op;
-                break;
-              }
-              if (t && _.label < t[2]) {
-                _.label = t[2];
-                _.ops.push(op);
-                break;
-              }
-              if (t[2]) _.ops.pop();
-              _.trys.pop();
-              continue;
-          }
-          op = body.call(thisArg, _);
-        } catch (e) {
-          op = [6, e];
-          y = 0;
-        } finally {
-          f = t = 0;
-        }
-      if (op[0] & 5) throw op[1];
-      return { value: op[0] ? op[1] : void 0, done: true };
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-  };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getNetworkConfig =
-  exports.deployUUPSArtifact =
-  exports.getProperConfig =
-  exports.waitForTxConfirmation =
-  exports.GAS_LIMIT_PATCH =
-    void 0;
+exports.getNetworkConfig = exports.deployUUPSArtifact = exports.getProperConfig = exports.waitForTxConfirmation = exports.GAS_LIMIT_PATCH = void 0;
 var config_1 = require("../deploy-config/config");
 exports.GAS_LIMIT_PATCH = 30000000;
 var waitForTxConfirmation = function (tx, confirmations) {
-  if (confirmations === void 0) {
-    confirmations = 1;
-  }
-  return __awaiter(void 0, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-      switch (_a.label) {
-        case 0:
-          return [4 /*yield*/, tx];
-        case 1:
-          return [2 /*return*/, _a.sent().wait(confirmations)];
-      }
+    if (confirmations === void 0) { confirmations = 1; }
+    return __awaiter(void 0, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, tx];
+                case 1: return [2 /*return*/, (_a.sent()).wait(confirmations)];
+            }
+        });
     });
-  });
 };
 exports.waitForTxConfirmation = waitForTxConfirmation;
 // Note that the deployments are saved as if the network name is localhost
 // See https://github.com/wighawag/hardhat-deploy#flags-1
 var getProperConfig = function (hre) {
-  var network = hre.network.name === "localhost" ? "hardhat" : hre.network.name;
-  return hre.config.networks[network];
+    var network = hre.network.name === "localhost" ? "hardhat" : hre.network.name;
+    return hre.config.networks[network];
 };
 exports.getProperConfig = getProperConfig;
 var deployUUPSArtifact = function (_a) {
-  var hre = _a.hre,
-    artifactBaseName = _a.artifactBaseName,
-    contract = _a.contract;
-  return __awaiter(void 0, void 0, void 0, function () {
-    var deployments, getNamedAccounts, deployer, deploy, deployImplResult, deployProxyResult;
-    return __generator(this, function (_b) {
-      switch (_b.label) {
-        case 0:
-          (deployments = hre.deployments), (getNamedAccounts = hre.getNamedAccounts);
-          return [4 /*yield*/, getNamedAccounts()];
-        case 1:
-          deployer = _b.sent().deployer;
-          deploy = deployments.deploy;
-          artifactBaseName = artifactBaseName || contract;
-          return [
-            4 /*yield*/,
-            deploy("".concat(artifactBaseName, "Impl"), {
-              contract: contract,
-              from: deployer,
-              gasLimit: exports.GAS_LIMIT_PATCH,
-            }),
-          ];
-        case 2:
-          deployImplResult = _b.sent();
-          console.log(
-            ""
-              .concat(contract, ", as ")
-              .concat(artifactBaseName, " implementation deployed at ")
-              .concat(deployImplResult.address),
-          );
-          return [
-            4 /*yield*/,
-            deploy("".concat(artifactBaseName, "Proxy"), {
-              contract: "ERC1967Proxy",
-              from: deployer,
-              gasLimit: exports.GAS_LIMIT_PATCH,
-              args: [deployImplResult.address, "0x"],
-            }),
-          ];
-        case 3:
-          deployProxyResult = _b.sent();
-          console.log("".concat(artifactBaseName, " ERC1967Proxy deployed at ").concat(deployProxyResult.address));
-          return [2 /*return*/];
-      }
+    var hre = _a.hre, artifactBaseName = _a.artifactBaseName, contract = _a.contract;
+    return __awaiter(void 0, void 0, void 0, function () {
+        var deployments, getNamedAccounts, deployer, deploy, deployImplResult, deployProxyResult;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    deployments = hre.deployments, getNamedAccounts = hre.getNamedAccounts;
+                    return [4 /*yield*/, getNamedAccounts()];
+                case 1:
+                    deployer = (_b.sent()).deployer;
+                    deploy = deployments.deploy;
+                    artifactBaseName = artifactBaseName || contract;
+                    return [4 /*yield*/, deploy("".concat(artifactBaseName, "Impl"), {
+                            contract: contract,
+                            from: deployer,
+                            gasLimit: exports.GAS_LIMIT_PATCH,
+                        })];
+                case 2:
+                    deployImplResult = _b.sent();
+                    console.log("".concat(contract, ", as ").concat(artifactBaseName, " implementation deployed at ").concat(deployImplResult.address));
+                    return [4 /*yield*/, deploy("".concat(artifactBaseName, "Proxy"), {
+                            contract: "ERC1967Proxy",
+                            from: deployer,
+                            gasLimit: exports.GAS_LIMIT_PATCH,
+                            args: [deployImplResult.address, "0x"],
+                        })];
+                case 3:
+                    deployProxyResult = _b.sent();
+                    console.log("".concat(artifactBaseName, " ERC1967Proxy deployed at ").concat(deployProxyResult.address));
+                    return [2 /*return*/];
+            }
+        });
     });
-  });
 };
 exports.deployUUPSArtifact = deployUUPSArtifact;
 var getNetworkConfig = function (_a) {
-  var network = _a.network;
-  return config_1.networkConfig[network];
+    var network = _a.network;
+    return config_1.networkConfig[network];
 };
 exports.getNetworkConfig = getNetworkConfig;
 //# sourceMappingURL=utils.js.map
